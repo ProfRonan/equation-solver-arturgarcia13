f40b37cb9bbe2bfcc0f151a6a16dbb0c27cb2c61
arturgarcia13 <128606880+arturgarcia13@users.noreply.github.com>
2023-04-03 14:42:09 +0000

    Primeira parte concluida

---
+++ main.py
@@ -0,0 +1,35 @@
+grauFun = int(input("Qual o grau da função? "))
+
+if grauFun < 1 or grauFun > 2:
+    print("Grau inválido")
+
+elif grauFun == 1:
+    print('A equação é do primeiro grau: \n')
+    coefAng = float(input("Qual o valor de 'a'? "))
+    
+    if coefAng == 0 :
+        print("Valor de a inválido")
+    else:
+        coefLin = float(input("Qual o valor de 'b'? "))
+    
+    fun1Solu = (-(coefLin)/coefAng) # .2f
+    print(f'{fun1Solu:.2f}')
+
+elif grauFun == 2:
+    print("A equação é do segundo grau: \n")
+    var_a = float(input("Qual o valor de 'a'? "))
+    if var_a == 0 :
+        print('Valor de a inválido')
+    else:
+        var_b = float(input("Qual o valor de 'b'? "))
+        var_c = float(input("Qual o valor de 'c'? "))
+        fun2Delta = (var_b**2) - (4*var_a*var_c)
+        if fun2Delta < 0 :
+            print("A equação não possui raízes reais")
+        elif fun2Delta == 0 :
+            print("A equação possui apenas uma raiz real")
+            fun2Raiz = (-(var_b) + fun2Delta**0.5) / (2*var_a)
+            print(f'{fun2Raiz:.2f}')
+
+
+
